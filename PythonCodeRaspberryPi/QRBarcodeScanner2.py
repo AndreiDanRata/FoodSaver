42ceab4b091caf862d82c884c7e727665f953e33
Andrei Rata <axr1023@cs.bham.ac.uk>
2022-09-25 17:00:55 +0000

    Added comments

--- PythonCodeRaspberryPi/QRBarcodeScanner2.py
+++ PythonCodeRaspberryPi/QRBarcodeScanner2.py
@@ -20,16 +20,13 @@ import argparse
 from pathlib import Path
 import RPi.GPIO as GPIO
 
+#Audio
 from pydub import AudioSegment
 from pydub.playback import play
-
 
 #For Api
 import requests
 import json
-
-
-
 
 
 #firebase
@@ -41,21 +38,13 @@ config = {
     }
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
+
 #pyrebase bug: to work with order_by_child I have to add this
 def noquote(s):
      return s
 pyrebase.pyrebase.quote = noquote
 
-
-
-# works with vs
-# soundLink = AudioSegment.from_mp3("LinkToAccount.mp3")
-# soundBeep = AudioSegment.from_mp3("Barcode-scanner-beep-sound.mp3")
-# soundLoggedIn = AudioSegment.from_mp3("UserLogged.mp3")
-# soundNotFound = AudioSegment.from_mp3("ItemNotFound.mp3")
-
-
-# works with vs
+# defining sounds
 soundLink = AudioSegment.from_wav("/home/andrei/Downloads/test/LinkToAccount.wav")
 soundBeep = AudioSegment.from_wav("/home/andrei/Downloads/test/Barcode-scanner-beep-sound.wav")
 soundLoggedIn = AudioSegment.from_wav("/home/andrei/Downloads/test/UserLogged.wav")
@@ -64,7 +53,6 @@ soundNotFound = AudioSegment.from_wav("/home/andrei/Downloads/test/ItemNotFound.
 #user authentification
 fle = Path('/home/andrei/Downloads/test/userFirebase.txt')
 fle.touch(exist_ok=True)  #if file does not exist, then create it.... otherwise do nothing
-
 userFile = open('/home/andrei/Downloads/test/userFirebase.txt', 'r')
 userFirebase = userFile.read()
 print(userFirebase)
@@ -72,15 +60,13 @@ userFile.close()
 
 if len(userFirebase) == 0 or len(userFirebase) == 1:
     play(soundLink)
-    print("nobody is authentificated")  #show red light
+    print("nobody is authentificated") 
 else:
-    
-    print("user is authentificated")      #show green light
-
-
+    print("user is authentificated")
 
 
 class MyScript:
+    #Gets data from barcode API
     def GetBarcodeApiData(barcodeData):
         #api example: https://api.upcdatabase.org/product/0111222333446?apikey=4368CFAB9E2F2B72DBBDA6EE20DD7877
         BARCODE_API_KEY ="4368CFAB9E2F2B72DBBDA6EE20DD7877"
@@ -90,11 +76,9 @@ class MyScript:
         response = requests.get(requestUrl)
 
         if(response.json()['success']==True):
-            #text=json.dumps(response.json(),sort_keys=True,indent=4)
             if response.json()['title'] != "":
                 print (response.json()['title'])
                 return response.json()['title']
-                
             elif response.json()['description'] != "":
                 print (response.json()['description'])
                 return response.json()['description']
@@ -102,23 +86,28 @@ class MyScript:
             print("Item not found")
             return 0
     
+    #Generates a random string of 10 chars
     def GetRandomKey():
         key=""
         alphabet = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
         for _ in range(10):
              key += choice(alphabet)
         return key
+
+    #Logins the user into the Firebase Account
     def loginUser(barcodeData):
         play(soundLoggedIn)
         print("User scanned their QR code")
         global userFirebase 
-        userFirebase = barcodeData.replace("userFirebaseUID:","")   #trim the input 
+        userFirebase = barcodeData.replace("userFirebaseUID:","")   
         file = open("/home/andrei/Downloads/test/userFirebase.txt", "w")
         file.write(userFirebase)
         file.close()
         userFile = open('/home/andrei/Downloads/test/userFirebase.txt', 'r')
         userFirebase = userFile.read()
         userFile.close()
+
+    #After a barcode is scanned this adds the data to the firebase database
     def addItemToDatabase(barcodeData):
         key =  MyScript.GetRandomKey()
         #if it is a qr code with the requested format
@@ -134,22 +123,23 @@ class MyScript:
             name = MyScript.GetBarcodeApiData(barcodeData)
             if name != 0:
                 play(soundBeep)
-                #expirationDate = date.today().strftime("%-d/%-m/%Y")
                 expirationDate = "11/11/2000"    
                 print(barcodeData)
+
                 #Add items to firebase database
                 db.child("foodItems").child(userFirebase).child(key).child("itemName").set(name)
                 db.child("foodItems").child(userFirebase).child(key).child("itemExpirationDate").set(expirationDate)
                 db.child("foodItems").child(userFirebase).child(key).child("key").set(key)
                 db.child("foodItems").child(userFirebase).child(key).child("toDonate").set(False)
                 print("successfuly added to database")
-                
             else:
-                play(soundNotFound) #add sound for item not found in the api
+                play(soundNotFound)
         else: 
-            play(soundNotFound)#add sound for item not found in the api
+            play(soundNotFound)
+    
+    #After a barcode is scanned this removes the data to the firebase database
     def removeItemFromDatabase(barcodeData):
-        
+        #if it is a qr code with the requested format
         if("Name/Expiration:" in barcodeData):
             play(soundBeep)
             splitData = barcodeData.split(sep="/+/", maxsplit=-1)
@@ -157,8 +147,8 @@ class MyScript:
             date = splitData[2]
             itemKey = ""
             for item in items.each():
-                itemVal=item.val() #the value
-                if(date == itemVal['itemExpirationDate']):  #the more recent date has bigger value
+                itemVal=item.val()
+                if(date == itemVal['itemExpirationDate']):
                     itemKey = item.key()
                     break
             if(itemKey != ""):
@@ -181,11 +171,12 @@ class MyScript:
                 if(shortestExpDateKey != ""):
                     print("The element (barcode) was removed from the database ")
                     db.child("foodItems").child(userFirebase).child(shortestExpDateKey).remove()
-                #print(db.child("foodItems").child(userFirebase).child(shortestExpDateKey).get().val())
             else:
-                play(soundNotFound) #add sound for item not found in the api
+                play(soundNotFound) 
         else: 
-            play(soundNotFound)#add sound for item not found in the api
+            play(soundNotFound)
+
+    #starts running the 2 camers on different threads to run in parrallel        
     class camThread(threading.Thread):
         def __init__(self, previewName, camID):
             threading.Thread.__init__(self)
@@ -199,6 +190,7 @@ class MyScript:
                 print ("Starting " + self.previewName)
                 MyScript.camrRemoveItems(self.previewName, self.camID)
 
+    #First camera adds items. Processing each frame happens here
     def camAddItems(previewName, camID):
         cv2.namedWindow(previewName)
         cam = cv2.VideoCapture(0, apiPreference=cv2.CAP_V4L2)
@@ -219,11 +211,6 @@ class MyScript:
         while rval:
             cv2.imshow(previewName, frame)
             rval, frame = cam.read()
-
-            # width = int(frame.shape[1] * 1)
-            # height = int(frame.shape[0] *1)
-            # frame = cv2.resize(frame, (width, height))
-            
             detections = pyzbar.decode(frame)                     
         
             for barcode in detections:
@@ -250,6 +237,7 @@ class MyScript:
         cam.release()
         cv2.destroyWindow(previewName)
 
+    #Second camera removes items. Processing each frame happens here
     def camrRemoveItems(previewName, camID):
         cv2.namedWindow(previewName)
         cam = cv2.VideoCapture(2, apiPreference=cv2.CAP_V4L2)
@@ -270,49 +258,12 @@ class MyScript:
         while rval:
             cv2.imshow(previewName, frame)
             rval, frame = cam.read()
-        
-            # width = int(frame.shape[1] * 1)
-            # height = int(frame.shape[0] *1)
-            # frame = cv2.resize(frame, (width, height))
-            
-
-
-
-            # #IMPORVE EFFICIENCY
-            # grey = cv2.cvtColor(cam, cv2.COLOR_BGR2GRAY)
-            # _, thresh =cv2.threshold(grey,120,255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-            
-            # kernel = np.ones((4, 4), np.uint8)  
-            # thresh = cv2.dilate(thresh, kernel, iterations=1)
-            # contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            
-            # bboxes = []
-
-
-            # for cnt in contours:
-            #     area = cv2.contourArea(cnt)
-            #     xmin, ymin, width, height = cv2.boundingRect(cnt)
-            #     extent = area / (width * height)
-            #     # filter non-rectangular objects and small objects
-            #     #if (extent > np.pi / 5) and (area > 50):
-            #     # min(width,height)/max(width,height)) > 0.2  and
-            #     if area > 400:
-            #         bboxes.append((xmin, ymin, xmin + width, ymin + height))
-            
-            
-            
-            # for xmin, ymin, xmax, ymax in bboxes:
-                
-            #     roi = cam[ymin:ymax, xmin:xmax]
-
-            detections = pyzbar.decode(frame)   #change to roi to use the optimization alg                  
+            detections = pyzbar.decode(frame)                  
             
             for barcode in detections:
-            
                 #create rectangle around the code
                 (x, y, w, h) = barcode.rect
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                
                 barcodeData = barcode.data.decode("utf-8")
                 
                 #authentificate user
@@ -328,9 +279,7 @@ class MyScript:
                         lastBarcodeDataRemoved = barcodeData
                         print("Item removed")
                 lastBarcodeData = barcodeData
-        
-        
-        
+
             key = cv2.waitKey(20)
             if key == 27:  # exit on ESC
                 break
@@ -339,9 +288,7 @@ class MyScript:
         
         
 
-
-
-# Create two threads as follows
+# Create two threads as follows 
 thread1 = MyScript.camThread("Camera Add", 0)
 thread2 = MyScript.camThread("Camera Remove", 1)
 thread1.start()
